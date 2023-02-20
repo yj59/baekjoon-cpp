229ce551df438a76f203c3792d8f484ee4fa1536
yj59 <yj.osoq@gmail.com>
2023-02-20 18:24:14 +0900

    no message

---
+++ 백준/Gold/9019. DSLR/DSLR.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int t, a, b, num, cal;
+int dist[10001];
+int pre[10001];
+char cmd[10001];
+bool visited[10001];
+queue<int> q;
+
+int main()
+{
+	scanf("%d", &t);
+	while (t--)
+	{
+		scanf("%d %d", &a, &b);
+        
+		// 각 text마다 배열 초기화
+		memset(dist, 0, sizeof(dist));
+		memset(pre, 0, sizeof(pre));
+		memset(cmd, 0, sizeof(cmd));
+		memset(visited, 0, sizeof(visited));
+
+		dist[a] = 0;
+		pre[a] = -1;
+		visited[a] = true;
+		q.push(a);
+
+		while (!q.empty())
+		{
+			num = q.front();
+			q.pop();
+			
+			// num*2
+			cal = num * 2;
+			if (cal > 9999) cal = cal % 10000;
+			if (visited[cal] == 0)
+			{
+				q.push(cal);
+				dist[cal] = dist[num] + 1;
+				pre[cal] = num;
+				cmd[cal] = 'D';
+				visited[cal] = 1;
+			}
+
+			// num-1
+			cal = num - 1;
+			if (cal == -1) cal = 9999;
+			if (visited[cal] == 0)
+			{
+				q.push(cal);
+				dist[cal] = dist[num] + 1;
+				pre[cal] = num;
+				cmd[cal] = 'S';
+				visited[cal] = 1;
+			}
+			
+			// 왼쪽으로 num 자릿수 이동
+			cal = (num % 1000) * 10 + num / 1000;
+			if (visited[cal] == 0)
+			{
+				q.push(cal);
+				dist[cal] = dist[num] + 1;
+				pre[cal] = num;
+				cmd[cal] = 'L';
+				visited[cal] = 1;
+				
+			}
+			
+			// 오른쪽으로 num 자릿수 이동
+			cal = (num / 10) + (num % 10) * 1000;
+			if (visited[cal] == 0)
+			{
+				q.push(cal);
+				dist[cal] = dist[num] + 1;
+				pre[cal] = num;
+				cmd[cal] = 'R';
+				visited[cal] = 1;
+			}
+		}
+		
+		// 문자열 초기화 및 결과값 할당
+		string result;
+		while (b != a)
+		{
+			result.push_back(cmd[b]);
+			b = pre[b];
+		}
+        
+		// 문자열 역출력
+		while (!result.empty())
+		{
+			printf("%c", result.back());
+			result.pop_back();
+		}
+		printf("\n");
+	}
+}
