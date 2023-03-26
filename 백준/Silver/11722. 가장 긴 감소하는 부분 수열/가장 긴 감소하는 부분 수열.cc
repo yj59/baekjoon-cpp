cdc5decbe3bccbf58ad0467725aef96b8aa4b6ea
Yejin An <93882395+yj59@users.noreply.github.com>
2023-03-26 19:36:55 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 8924 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int n;
+int A[1000001];
+int R[1000001];
+int main() {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) scanf("%d", &A[i]);
+	int r = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (r == 0 || R[r - 1] > A[i])
+		{
+			R[r++] = A[i];
+			continue;
+		}
+		int st = 0;
+		int end = r;
+		while (st <= end)
+		{
+			int mid = (st + end) / 2;
+			if (R[mid] <= A[i]) end = mid - 1;
+			else st = mid + 1;
+		}
+		R[st] = A[i];
+	}
+	printf("%d\n", r);
+	return 0;
+}
