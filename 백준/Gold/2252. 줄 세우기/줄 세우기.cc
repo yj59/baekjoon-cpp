4a0aabf9609b331ab99e90005f73f71eb4d3a9c2
Yejin An <93882395+yj59@users.noreply.github.com>
2023-03-26 19:22:16 +0900

    [Gold III] Title: 줄 세우기, Time: 44 ms, Memory: 3824 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define MAX 32002
+using namespace std;
+vector<int> AM[MAX];
+int Degree[MAX] = { 0, };
+int n, m, a, b;
+
+int main()
+{
+	queue<int> q;
+
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < m; i++)
+	{
+		scanf("%d %d", &a, &b);
+		AM[a].push_back(b);
+		Degree[b]++;
+	}
+
+	for (int i = 1; i <= n; i++)
+		if (Degree[i] == 0) q.push(i);
+
+	while (!q.empty())
+	{
+		a = q.front();
+		q.pop();
+
+		printf("%d ", a);
+
+		for (int i = 0; i < AM[a].size(); i++)
+		{
+			b = AM[a][i];
+			Degree[b]--;
+			if (Degree[b] == 0) q.push(b);
+		}
+	}
+	return 0;
+}
