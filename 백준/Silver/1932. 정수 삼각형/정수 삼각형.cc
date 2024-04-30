ef82ce06c2a0dd5a4dbcb289a10ca5b06a6abc9c
Yejin An <93882395+yj59@users.noreply.github.com>
2024-04-30 18:42:16 +0900

    [Silver I] Title: 정수 삼각형, Time: 40 ms, Memory: 3000 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int dp[501][501];
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cin >> dp[i][j];
+		}
+	}
+
+	int sum = -1;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			if (j == 1)
+			{
+				dp[i][j] += dp[i - 1][j];
+			}
+			else
+			{
+				dp[i][j] += max(dp[i - 1][j - 1], dp[i - 1][j]);
+			}
+			if (sum < dp[i][j]) sum = dp[i][j];
+		}
+	}
+
+	cout << sum;
+	return 0;
+}
