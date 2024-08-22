f14428bfac080d1657bf40d9525c83aa942049b2
Yejin An <93882395+yj59@users.noreply.github.com>
2024-08-22 21:04:39 +0900

    [Gold V] Title: 카드게임, Time: 40 ms, Memory: 17740 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10835. 카드게임/카드게임.cc
+++ 백준/Gold/10835. 카드게임/카드게임.cc
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cstring>
 #include <cmath>
 using namespace std;
 
-int dp[2001][2001] = { 0, };
+int dp[2001][2001];
 int lc[2001];
 int rc[2001];
 int n;
@@ -17,7 +18,9 @@ void input()
 int recur(int l, int r)
 {
 	if (l == n || r == n) return 0;
-	if (dp[l][r]) return dp[l][r];
+	if (dp[l][r] != -1) return dp[l][r];
+
+	dp[l][r] = 0;
 
 	if (lc[l] <= rc[r]) return dp[l][r] = max(recur(l + 1, r), recur(l + 1, r + 1));
 	if (lc[l] > rc[r]) return dp[l][r] = max(dp[l][r], recur(l, r + 1) + rc[r]);
@@ -27,6 +30,8 @@ int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+
+	memset(dp, -1, sizeof(dp));
 
 	input();
 	cout << recur(0, 0);
