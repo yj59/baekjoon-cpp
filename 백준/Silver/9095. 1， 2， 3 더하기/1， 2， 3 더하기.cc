9d4ec732568fb4f48310286c70a907d22e0c0d1e
Yejin An <93882395+yj59@users.noreply.github.com>
2024-08-29 13:32:56 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,20 +1,29 @@
 #include <iostream>
 using namespace std;
-int d[11];
-int n, i, c;
+
+int arr[12];
+
 int main()
 {
-    scanf("%d", &n);
-    d[1] = 1; d[2] = 2; d[3] = 4;
-    for (i = 4; i < 11; i++)
-    {
-        d[i] = d[i - 1] + d[i - 2];
-        d[i] += d[i - 3];
-    }
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &c);
-        printf("%d\n", d[c]);
-    }
-    return 0;
+	int tc;
+	cin >> tc;
+	
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[3] = 4;
+
+	for (int i = 4; i < 12; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+	}
+
+	while (tc--)
+	{
+		int n;
+		cin >> n;
+
+		cout << arr[n] << "\n";
+	}
+
+	return 0;
 }
