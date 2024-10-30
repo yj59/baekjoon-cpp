df460c723e48ec47221d6fb2edc540ed679ea4cf
Yejin An <93882395+yj59@users.noreply.github.com>
2024-10-30 12:58:51 +0900

    [Gold V] Title: 회장뽑기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int fw[51][51];
+vector<int> v;
+
+int floydWarshall(int n)
+{
+	for (int k = 1; k <= n; k++)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				if (i == k || k == j) continue;
+				if (fw[i][k] + fw[k][j] < fw[i][j]) 
+				{
+					fw[i][j] = fw[i][k] + fw[k][j];
+				}
+			}
+		}
+	}
+
+	int res = 1e9;
+	for (int i = 1; i <= n; i++)
+	{
+		int score = 0;
+		for (int j = 1; j <= n; j++)
+		{
+			score = max(score, fw[i][j]);
+		}
+		if (score < res)
+		{
+			res = score;
+			v.clear();
+			v.push_back(i);
+		}
+		else if (score == res)
+		{
+			v.push_back(i);
+		}
+	}
+
+	return res;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	// input & init
+	int n;
+	cin >> n;
+
+	fill(&fw[0][0], &fw[n][n + 1], 1e9);
+	for (int i = 0; i <= n; i++)
+	{
+		fw[i][i] = 0;
+	}
+
+	while(1)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		if (a == -1 && b == -1) break;
+
+		fw[a][b] = 1;
+		fw[b][a] = 1;
+	}
+
+	// solve
+	cout << floydWarshall(n) << " " << v.size() << "\n";
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
+	}
+
+	return 0;
+}
