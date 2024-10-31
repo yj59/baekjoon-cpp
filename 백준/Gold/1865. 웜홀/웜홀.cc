a96b05699ac6cdaf9e4ff407ef38b1ab167d89bb
Yejin An <93882395+yj59@users.noreply.github.com>
2024-10-31 23:34:24 +0900

    [Gold III] Title: 웜홀, Time: 20 ms, Memory: 2332 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -2,14 +2,46 @@
 #include <vector>
 using namespace std;
 
-struct Edge 
+struct Edge
 {
 	int from, to, cost;
 };
 
 vector<Edge> edges;
 
-int main() 
+bool bellmanFord(int n)
+{
+	vector<int> dist(n + 1, 1e9);
+	dist[1] = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int pos = 0; pos < edges.size(); pos++)
+		{
+			int from = edges[pos].from;
+			int to = edges[pos].to;
+			int cost = edges[pos].cost;
+
+			if (dist[from] + cost < dist[to])
+			{
+				dist[to] = dist[from] + cost;
+			}
+		}
+	}
+
+	for (int pos = 0; pos < edges.size(); pos++)
+	{
+		int from = edges[pos].from;
+		int to = edges[pos].to;
+		int cost = edges[pos].cost;
+
+		if (dist[to] > dist[from] + cost) return 1;
+	}
+
+	return 0;
+}
+
+int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -26,54 +58,25 @@ int main()
 		int n, m, w;
 		cin >> n >> m >> w;
 
-		for (int i = 0; i < m; i++) 
+		for (int i = 0; i < m; i++)
 		{
 			int s, e, t;
 			cin >> s >> e >> t;
+
 			edges.push_back({ s, e, t });
 			edges.push_back({ e, s, t });
 		}
-		for (int i = 0; i < w; i++) 
+
+		for (int i = 0; i < w; i++)
 		{
 			int s, e, t;
 			cin >> s >> e >> t;
+
 			edges.push_back({ s, e, -t });
 		}
 
-		vector<int> dist(n + 1, 1e9);
-		dist[1] = 0;
-		bool flag = 0;
-
-		// solve (Bellman Ford)
-		for (int i = 0; i < n; i++)
-		{
-			for (int pos = 0; pos < edges.size(); pos++)
-			{
-				int from = edges[pos].from;
-				int to = edges[pos].to;
-				int cost = edges[pos].cost;
-
-				if (dist[from] + cost < dist[to])
-				{
-					dist[to] = dist[from] + cost;
-				}
-			}
-		}
-
-		for (int pos = 0; pos < edges.size(); pos++)
-		{
-			int from = edges[pos].from;
-			int to = edges[pos].to;
-			int cost = edges[pos].cost;
-
-			if (dist[to] > dist[from] + cost)
-			{
-				flag = 1;
-			}
-		}
-
-		// output
-		if (flag) cout << "YES\n";
+		// solve
+		if (bellmanFord(n)) cout << "YES\n";
 		else cout << "NO\n";
 	}
 
