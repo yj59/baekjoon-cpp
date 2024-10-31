7c3d0c47855068f24f8be4d495be5dc365a6a04b
Yejin An <93882395+yj59@users.noreply.github.com>
2024-10-31 23:30:14 +0900

    [Gold III] Title: 웜홀, Time: 20 ms, Memory: 2332 KB -BaekjoonHub

---
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Edge 
+{
+	int from, to, cost;
+};
+
+vector<Edge> edges;
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	// test
+	int test_case;
+	cin >> test_case;
+
+	for (int tc = 0; tc < test_case; tc++)
+	{
+		// input & init
+		edges.clear();
+
+		int n, m, w;
+		cin >> n >> m >> w;
+
+		for (int i = 0; i < m; i++) 
+		{
+			int s, e, t;
+			cin >> s >> e >> t;
+			edges.push_back({ s, e, t });
+			edges.push_back({ e, s, t });
+		}
+		for (int i = 0; i < w; i++) 
+		{
+			int s, e, t;
+			cin >> s >> e >> t;
+			edges.push_back({ s, e, -t });
+		}
+
+		vector<int> dist(n + 1, 1e9);
+		dist[1] = 0;
+		bool flag = 0;
+
+		// solve (Bellman Ford)
+		for (int i = 0; i < n; i++)
+		{
+			for (int pos = 0; pos < edges.size(); pos++)
+			{
+				int from = edges[pos].from;
+				int to = edges[pos].to;
+				int cost = edges[pos].cost;
+
+				if (dist[from] + cost < dist[to])
+				{
+					dist[to] = dist[from] + cost;
+				}
+			}
+		}
+
+		for (int pos = 0; pos < edges.size(); pos++)
+		{
+			int from = edges[pos].from;
+			int to = edges[pos].to;
+			int cost = edges[pos].cost;
+
+			if (dist[to] > dist[from] + cost)
+			{
+				flag = 1;
+			}
+		}
+
+		// output
+		if (flag) cout << "YES\n";
+		else cout << "NO\n";
+	}
+
+	return 0;
+}
