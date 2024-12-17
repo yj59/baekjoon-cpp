c65d416c52956870a1f0605aab2d7b30180381bd
Yejin An <93882395+yj59@users.noreply.github.com>
2024-12-17 14:37:51 +0900

    [Gold V] Title: 토마토, Time: 92 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,86 +1,90 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
+#include <cmath>
 #include <queue>
-
+#include <tuple>
 using namespace std;
 
-int dz[6] = { 0, 0, 0, 0, 1, -1 };
-int dx[6] = { 0, 0, 1, -1, 0, 0 };
-int dy[6] = { 1, -1, 0, 0, 0, 0 };
+int dx[6] = { -1, 0, 1, 0, 0, 0 };
+int dy[6] = { 0, -1, 0, 1, 0, 0 };
+int dz[6] = { 0, 0, 0, 0, -1, 1 };
 
-int tomato[102][102][102];
-int d[102][102][102];
+int box[101][101][101];
+int visited[101][101][101];
+queue<tuple<int, int, int>> q;
 
+int solve(int n, int m, int h)
+{
+	// bfs
+	while (!q.empty())
+	{
+		tuple<int, int, int> t = q.front();
+		int z = get<0>(t);
+		int y = get<1>(t);
+		int x = get<2>(t);
+		q.pop();
+
+		for (int i = 0; i < 6; i++)
+		{
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			int nz = z + dz[i];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h) continue;
+			if (box[nz][ny][nx] == 0)
+			{
+				box[nz][ny][nx] = box[z][y][x] + 1;
+				q.push({ nz, ny, nx });
+			}
+		}
+	}
+
+	// calc
+	int day = 0;
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			for (int k = 0; k < n; k++)
+			{
+				if (box[i][j][k] == 0)
+				{
+					return -1;
+				}
+
+				day = max(day, box[i][j][k]);
+			}
+		}
+	}
+
+	return day - 1;
+}
 
 int main()
 {
-    queue<pair<pair<int, int>, int>> q;
-    int n, m, h;
-    int r = 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    scanf("%d %d %d", &m, &n, &h);
+	// input
+	int n, m, h;
+	cin >> n >> m >> h;
 
-    for (int i = 0; i < h; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            for (int k = 0; k < m; k++)
-            {
-                scanf("%d", &tomato[i][j][k]);
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			for (int k = 0; k < n; k++)
+			{
+				cin >> box[i][j][k];
+				if (box[i][j][k] == 1)
+				{
+					q.push({ i, j, k });
+				}
+			}
+		}
+	}
 
-                if (tomato[i][j][k] == 1)
-                {
-                    q.push({ {i, j}, k });
-                }
-                if (tomato[i][j][k] == 0)
-                {
-                    d[i][j][k] = -1;
-                }
-            }
-        }
-    }
+	// output
+	cout << solve(n, m, h);
 
-    while (!q.empty())
-    {
-        int x, y, z;
-        z = q.front().first.first;
-        x = q.front().first.second;
-        y = q.front().second;
-
-        q.pop();
-
-        for (int i = 0; i < 6; i++)
-        {
-            int nx, ny, nz;
-
-            nz = z + dz[i];
-            nx = x + dx[i];
-            ny = y + dy[i];
-
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h) continue;
-            if (d[nz][nx][ny] >= 0) continue;
-            d[nz][nx][ny] = d[z][x][y] + 1;
-            q.push({ { nz, nx }, ny });
-        }
-    }
-
-    for (int i = 0; i < h; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            for (int k = 0; k < m; k++)
-            {
-                if (d[i][j][k] == -1)
-                {
-                    printf("-1\n");
-                    return 0;
-                }
-                r = max(r, d[i][j][k]);
-            }
-        }
-    }
-
-    printf("%d\n", r);
-    return 0;
+	return 0;
 }
