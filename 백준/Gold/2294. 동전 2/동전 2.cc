5924c5058e904621f2e5c99d6cd279b64f846f52
Yejin An <93882395+yj59@users.noreply.github.com>
2025-01-04 16:16:56 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,34 +1,30 @@
 #include <iostream>
-#include <algorithm>
+#include <cmath>
 using namespace std;
-
-int a[10001];
 int dp[10001];
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(NULL);	cout.tie(NULL);
 
 	// input & init
 	int n, k;
 	cin >> n >> k;
-
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> a[i];
-	}
 	fill(dp + 1, dp + k + 1, 1e9);
 
-	// solve
-	for (int i = 1; i <= n; i++)
+	// dynamic programming
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = a[i]; j <= k; j++)
+		int coin;
+		cin >> coin;
+
+		for (int j = coin; j <= k; j++)
 		{
-			dp[j] = min(dp[j], dp[j - a[i]] + 1);
+			dp[j] = min(dp[j], dp[j - coin] + 1);
 		}
 	}
-
+	
 	// output
 	if (dp[k] == 1e9) cout << -1;
 	else cout << dp[k];
