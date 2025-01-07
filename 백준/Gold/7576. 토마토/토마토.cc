0ca5acbce49f6c391c6108ddff239dab42d3662b
Yejin An <93882395+yj59@users.noreply.github.com>
2025-01-07 13:39:18 +0900

    [Gold V] Title: 토마토, Time: 72 ms, Memory: 10216 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int dx[] = { 0, 1, 0, -1 };
+int dy[] = { -1, 0, 1, 0 };
+
+int box[1001][1001];
+queue<pair<int, int>> q;
+
+void bfs(int n, int m)
+{
+	while(!q.empty())
+	{
+		int x = q.front().second;
+		int y = q.front().first;
+
+		q.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || ny < 0 || nx >= m || ny >= n || box[ny][nx] != 0) continue;
+
+			box[ny][nx] = box[y][x] + 1;
+			q.push({ ny, nx });
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// init & input
+	int m, n;
+	cin >> m >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> box[i][j];
+			if (box[i][j] == 1) q.push({ i, j });
+		}
+	}
+
+	//solve
+	bfs(n, m);
+
+	int cnt = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (box[i][j] == 0)
+			{
+				cout << -1;
+				return 0;
+			}
+
+			cnt = max(cnt, box[i][j]);
+		}
+	}
+
+	// ouptput
+	cout << cnt - 1;
+
+	return 0;
+}
