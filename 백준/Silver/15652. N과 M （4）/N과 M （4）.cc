56a15e7a7e0a9df4dd4eae2fb8ba440a5b5e7db2
Yejin An <93882395+yj59@users.noreply.github.com>
2025-04-10 19:37:00 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int seq[9];
+bool visited[9];
+int n, m;
+
+void dfs(int cur, int cnt)
+{
+	
+	if (cnt == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << seq[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = cur; i <= n; i++)
+	{
+		seq[cnt] = i;
+
+		visited[i] = 1;
+		dfs(i, cnt + 1);
+		visited[i] = 0;
+	}
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+	dfs(1, 0);
+
+	return 0;
+}
