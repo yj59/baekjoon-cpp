42d8233a798e2febcdc61f43cc52d11073a0922b
Yejin An <93882395+yj59@users.noreply.github.com>
2025-04-19 18:04:00 +0900

    [Gold V] Title: 토마토, Time: 92 ms, Memory: 7640 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,26 +1,40 @@
 #include <iostream>
-#include <cmath>
 #include <queue>
 #include <tuple>
 using namespace std;
 
-int dx[6] = { -1, 0, 1, 0, 0, 0 };
-int dy[6] = { 0, -1, 0, 1, 0, 0 };
-int dz[6] = { 0, 0, 0, 0, -1, 1 };
+int dx[] = { 0, 1, 0, -1, 0, 0 };
+int dy[] = { -1, 0, 1, 0, 0, 0 };
+int dz[] = { 0, 0, 0, 0, -1, 1 };
 
 int box[101][101][101];
-int visited[101][101][101];
 queue<tuple<int, int, int>> q;
+int n, m, h;
 
-int solve(int n, int m, int h)
+void input()
 {
-	// bfs
+	cin >> m >> n >> h;
+
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			for (int k = 0; k < m; k++)
+			{
+				cin >> box[i][j][k];
+				if (box[i][j][k] == 1) q.push({ i, j, k });
+			}
+		}
+	}
+}
+
+void solve()
+{
 	while (!q.empty())
 	{
-		tuple<int, int, int> t = q.front();
-		int z = get<0>(t);
-		int y = get<1>(t);
-		int x = get<2>(t);
+		int z = get<0>(q.front());
+		int y = get<1>(q.front());
+		int x = get<2>(q.front());
 		q.pop();
 
 		for (int i = 0; i < 6; i++)
@@ -29,34 +43,35 @@ int solve(int n, int m, int h)
 			int ny = y + dy[i];
 			int nz = z + dz[i];
 
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h) continue;
-			if (box[nz][ny][nx] == 0)
-			{
-				box[nz][ny][nx] = box[z][y][x] + 1;
-				q.push({ nz, ny, nx });
-			}
+			if (nx < 0 || nx >= m || ny < 0 || ny >= n || nz < 0 || nz >= h || box[nz][ny][nx] != 0) continue;
+			box[nz][ny][nx] = box[z][y][x] + 1;
+			q.push({ nz, ny, nx });
 		}
 	}
+	
+}
 
-	// calc
-	int day = 0;
+void output()
+{
+	int cnt = 0;
 	for (int i = 0; i < h; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < n; j++)
 		{
-			for (int k = 0; k < n; k++)
+			for (int k = 0; k < m; k++)
 			{
 				if (box[i][j][k] == 0)
 				{
-					return -1;
+					cout << -1;
+					return;
 				}
-
-				day = max(day, box[i][j][k]);
+				cnt = max(cnt, box[i][j][k]);
 			}
 		}
 	}
 
-	return day - 1;
+	cout << cnt - 1;
+	return;
 }
 
 int main()
@@ -64,27 +79,9 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	// input
-	int n, m, h;
-	cin >> n >> m >> h;
-
-	for (int i = 0; i < h; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			for (int k = 0; k < n; k++)
-			{
-				cin >> box[i][j][k];
-				if (box[i][j][k] == 1)
-				{
-					q.push({ i, j, k });
-				}
-			}
-		}
-	}
-
-	// output
-	cout << solve(n, m, h);
-
+	input();
+	solve();
+	output();
+	
 	return 0;
 }
