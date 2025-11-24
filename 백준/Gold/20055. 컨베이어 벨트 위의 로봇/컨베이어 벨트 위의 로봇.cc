986199f344c4dda3be8e261947526dead11e4462
Yejin An <93882395+yj59@users.noreply.github.com>
2025-11-24 16:17:09 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 92 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
@@ -1,104 +1,69 @@
 #include <iostream>
+#include <deque>
+
 using namespace std;
 
-int a[202];
-bool robot[202] = { 0, };
-int up, down, cnt;
-bool front;
+deque<bool> robot;
+deque<int> belt;
+int n, k;
 
-void unload()
+int durability()
 {
-	if (!robot[down]) return;
-
-	robot[down] = false;
-	return;
-}
-
-void rotation(int n)
-{
-	if (up <= 1) up = 2 * n;
-	else up--;
-
-	if (down <= 1) down = 2 * n;
-	else down--;
-
-	unload();
-}
-
-void move(int n)
-{
-	if (!front) return;
-
-	int cur = down;
-	for (int i = 1; i < n; i++)
+	int n = 0;
+	for (int i = 0; i < belt.size(); i++)
 	{
-		int cur = down - i;
-		if (cur <= 0) cur = 2 * n + (down - i);
+		if (!belt[i]) n++;
+	}
+	return n;
+}
 
-		if (!robot[cur]) continue;
+int solve()
+{
+	int level = 0;
+	while (durability() < k)
+	{
+		level++;
 
-		int next = cur + 1;
-		if (next > 2 * n) next = 1;
-		if (!a[next] || robot[next]) continue;
+		belt.push_front(belt.back());
+		belt.pop_back();
 
-		robot[cur] = false;
-		robot[next] = true;
-		a[next]--;
+		robot.push_front(robot.back());
+		robot.pop_back();
 
-		if (!a[next]) cnt++;
+		if (robot[n - 1]) robot[n - 1] = 0;
+
+		for (int i = n - 2; i >= 0; i--)
+		{
+			if (robot[i] && !robot[i + 1] && belt[i + 1] >= 1)
+			{
+				robot[i] = 0;
+				belt[i + 1]--;
+				if (i + 1 == n - 1) continue;
+				robot[i + 1] = 1;
+			}
+		}
+
+		if (belt[0] >= 1 && !robot[0])
+		{
+			belt[0]--;
+			robot[0] = 1;
+		}
 	}
 
-	unload();
-	return;
-}
-
-void load(int n)
-{
-	if (a[up] == 0 || robot[up] == 1) return;
-
-	robot[up] = true;
-	a[up]--;
-
-	if (!front) front = 1;
-	if (a[up] == 0) cnt++;
-}
-
-int solve(int n, int k)
-{
-	cnt = 0;
-	up = 1;
-	down = n;
-	front = false;
-
-	int step = 1;
-
-	while (1)
-	{
-		rotation(n);
-		move(n);
-		load(n);
-
-		if (cnt >= k) return step;
-
-		step++;
-	}
-
-	return -1;
+	return level;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
-	int n, k;
 	cin >> n >> k;
-
-	for (int i = 1; i <= 2 * n; i++)
+	for (int i = 0; i < 2 * n; i++)
 	{
-		cin >> a[i];
+		int el;
+		cin >> el;
+		belt.push_back(el);
+		robot.push_back(0);
 	}
 
-	cout << solve(n, k);
+	cout << solve();
 	return 0;
 }
