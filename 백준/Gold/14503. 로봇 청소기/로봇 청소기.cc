fb2bf40b71dffa09c0162ed7496a05f73d28fe0d
Yejin An <93882395+yj59@users.noreply.github.com>
2025-11-24 13:00:21 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -1,72 +1,68 @@
 #include <iostream>
 using namespace std;
 
-bool room[50][50];
-bool visited[50][50] = { 0, };
-int dx[4] = { -1, 0, 1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
+int dx[] = { 0, 1, 0, -1 }; // 북 동 남 서
+int dy[] = { -1, 0, 1, 0 };
 
-int N, M;
-int cnt;
-int r, c, d;
+int map[51][51];
+int n, m;
+int cnt = 0;
 
-void dfs()
+int turn(int d)
 {
-    for (int i = 0; i < 4; i++)
-    {
-        int nd = (d + 3 - i) % 4;
-        int nr = r + dx[nd];
-        int nc = c + dy[nd];
+	return (d + 3) % 4;
+}
 
-        if (nr < 0 || nr >= N || nc < 0 || nc >= M || room[nr][nc] == 1) continue;
-        
-        if (room[nr][nc] == 0 && visited[nr][nc] == 0)
-        {
-            visited[nr][nc] = 1;
-            r = nr;
-            c = nc;
-            d = nd;
-            cnt++;
-            dfs();
-        }
-    }
-    int pd;
-    if (d > 1) pd = d - 2;
-    else pd = d + 2;
-    int pr = r + dx[pd];
-    int pc = c + dy[pd];
-    if (pr >= 0 && pr <= N || pc >= 0 || pc <= M)
-    {
-        if (room[pr][pc] == 0)
-        {
-            r = pr;
-            c = pc;
-            dfs();
-        }
-        else
-        {
-            cout << cnt;
-            exit(0);
-        }
-    }
+void clean(int y, int x, int d)
+{
+	if (map[y][x] == 0)
+	{
+		map[y][x] = -1;
+		cnt++;
+	}
+
+	int nd = d;
+	for (int i = 0; i < 4; i++)
+	{
+		nd = turn(nd);
+		int nx = x + dx[nd];
+		int ny = y + dy[nd];
+
+		if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+		if (map[ny][nx] == 0)
+		{
+			clean(ny, nx, nd);
+			return;
+		}
+	}
+
+	int bx = x - dx[d];
+	int by = y - dy[d];
+
+	if (by < 0 || by >= n || bx < 0 || bx >= m) return;
+	if (map[by][bx] == 1) return;
+
+	clean(by, bx, d);
 }
 
 int main()
 {
-    cin >> N >> M;
-    cin >> r >> c >> d;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            cin >> room[i][j];
-        }
-    }
+	
+	cin >> n >> m;
+	int r, c, d;
+	cin >> r >> c >> d;
 
-    visited[r][c] = 1;
-    cnt++;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> map[i][j];
+		}
+	}
 
-    dfs();
+	clean(r, c, d);
 
-    return 0;
+	cout << cnt;
+	return 0;
 }
